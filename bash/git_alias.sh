1e983838ef0dd06607e4bf53df8bd44dc2a882f6
nitehogg <keith.b.elliott@gmail.com>
2026-01-25 21:24:47 -0500

    updates git alias

--- bash/git_alias.sh
+++ bash/git_alias.sh
@@ -1,3 +1,7 @@
+function set_upsteam_origin() {
+  git push --set-upstream origin "$(git branch --show-current)"
+}
+
 alias gss="git status"
 alias gps="git push"
 alias gpl="git pull"
@@ -5,4 +9,4 @@ alias gpo="git pull origin "
 alias gc="git commit -am"
 alias gs="git stash"
 alias gsp="git stash pop"
-alias gpu="git push --set-upstream origin $(git branch --show-current)"
+alias gpu="set_upsteam_origin"
--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -3,6 +3,7 @@ return {
     version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
+        -- require("nvim-surround").set_keymaps({ buffer = true })
         require("nvim-surround").setup({
             -- Configuration here, or leave empty to use defaults
         })
