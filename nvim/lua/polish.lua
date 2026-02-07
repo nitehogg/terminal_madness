987b066859f0430d155ada096a68ada68377f166
nitehogg <keith.b.elliott@gmail.com>
2026-02-07 03:12:13 -0500

    adding 80 col marker. session restore

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,5 +1,13 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+vim.opt.colorcolumn = "80"
+vim.cmd [[highlight ColorColumn ctermbg=DarkGrey guibg=#2a2a2a]]
 
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+
+
+
+
+
+
+
+
+
+
